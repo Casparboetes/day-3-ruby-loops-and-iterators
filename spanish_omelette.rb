bc759e02650bd62801357e5cabcb04622f83ff51
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2017-12-07 08:29:27 +0100

    Print out the Ingredients

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -1,4 +1,22 @@
-# Basic elements to work with
+# Helper methods
+
+def print_divider
+  puts "*" * 40
+  puts "\n"
+end
+
+def print_progress_bar
+  3.times { sleep 0.5; print "." }
+  puts "\n"
+end
+
+
+# Defining the Ingredients
+
+# These could also have been written like @num_potatoes = 5 and @num_eggs = 6.
+# By doing that, you can also make your variables accessible any place along your program, the same way you do by prefixing them with the @ symbol.
+# In this case they are written as CONSTANTS.
+
 
 NUM_POTATOES = 5
 NUM_EGGS = 6
@@ -12,14 +30,13 @@ ingredients = [
 ]
 
 
-# Helper methods
+# Printing Out the Ingredients
 
-def print_divider
-  puts "*" * 40
-  puts "\n"
-end
+puts "*    Let's cook a Spanish Omelette!    *"
+print_divider
 
-def print_progress_bar
-  3.times { sleep 0.5; print "." }
-  puts "\n"
+puts "You need the following ingredients:"
+
+ingredients.each do |ingredient|
+  puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
 end
